bc42cbf774b4b6c38d4d7ae64e36ed530c8abd99
Johnny Ekparam <johnnye4u2c@gmail.com>
2023-07-25 00:37:48 +0100

    added tab for desktop views

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,3 +1,4 @@
+// ? Carousel starts
 const slidesContainer = document.querySelector(".slide-container");
 const slides = document.querySelectorAll(".slide");
 const indicatorsContainer = document.querySelector(".indicators");
@@ -41,3 +42,35 @@ function nextSlide() {
 }
 
 setInterval(nextSlide, 5000); // Auto slide every 5 seconds
+// ? Carousel ends
+
+// ? tab starts
+function openTab(evt, tabName) {
+  var i, tabContent, tabButtons;
+  tabContent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = "none";
+  }
+  tabButtons = document.getElementsByClassName("tab-button");
+  for (i = 0; i < tabButtons.length; i++) {
+    tabButtons[i].classList.remove("active");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.classList.add("active");
+}
+
+// Set Tab 1 as the default selected tab
+document.getElementById("tabButton1").addEventListener("click", function () {
+  openTab(event, "tab1");
+});
+
+document.getElementById("tabButton2").addEventListener("click", function () {
+  openTab(event, "tab2");
+});
+
+// Set Tab 1 as the default selected tab
+document.getElementById("tab1").style.display = "block";
+document.querySelector(".tab-button.active").classList.remove("active");
+document.getElementById("tabButton1").classList.add("active");
+
+// ? accordion starts
