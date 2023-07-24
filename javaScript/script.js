27b64f825223f20a72e4b222cd680b9482be7a84
Johnny Ekparam <johnnye4u2c@gmail.com>
2023-07-24 19:27:32 +0100

    added carousel

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,6 +1,43 @@
-// JavaScript to toggle accordion functionality on mobile
-const navLinks = document.querySelector(".nav-links");
+const slidesContainer = document.querySelector(".slide-container");
+const slides = document.querySelectorAll(".slide");
+const indicatorsContainer = document.querySelector(".indicators");
 
-navLinks.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
+let currentIndex = 0;
+const slideWidth = slides[0].offsetWidth; // Get the width of a single slide
+const slideCount = slides.length;
+
+function updateSlidePosition() {
+  slidesContainer.style.transform = `translateX(-${
+    currentIndex * slideWidth
+  }px)`;
+}
+
+function updateIndicators() {
+  const indicators = document.querySelectorAll(".indicator");
+  indicators.forEach((indicator, index) => {
+    indicator.classList.toggle("active", index === currentIndex);
+  });
+}
+
+function goToSlide(index) {
+  currentIndex = index;
+  updateSlidePosition();
+  updateIndicators();
+}
+
+slides.forEach((slide, index) => {
+  const indicator = document.createElement("div");
+  indicator.classList.add("indicator");
+  indicator.addEventListener("click", () => goToSlide(index));
+  indicatorsContainer.appendChild(indicator);
 });
+
+updateIndicators();
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % slideCount;
+  updateSlidePosition();
+  updateIndicators();
+}
+
+setInterval(nextSlide, 5000); // Auto slide every 5 seconds
