58824187da88aa4d3d56aa1285bc8b52c4d8840e
Johnny Ekparam <johnnye4u2c@gmail.com>
2023-07-24 13:13:28 +0100

    testing something go back to previous commit

---
+++ javaScript/script.js
@@ -0,0 +1,6 @@
+// JavaScript to toggle accordion functionality on mobile
+const navLinks = document.querySelector(".nav-links");
+
+navLinks.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
